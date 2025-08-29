736e74358b3a4c473fd88199be52e4bb01140e6a
AdityaNegi3 <adi.negi730@gmail.com>
2025-08-29 12:01:32 +0530

    newlogo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ const Header: React.FC<HeaderProps> = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur-lg border-b border-white/10 transition-all duration-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        {/* Line 1: Logo Centered (image from /public/logo.png) */}
+        {/* Line 1: Logo Centered (image from /public/logo123.png) */}
         <div className="flex justify-center">
           <Link
             to="/"
@@ -41,7 +41,7 @@ const Header: React.FC<HeaderProps> = () => {
                        hover:bg-white/5 hover:ring-yellow-400/40"
           >
             <img
-              src="/logo.png"
+              src="/logo123.png"
               alt="MONAARC logo"
               loading="eager"
               className="h-10 md:h-12 w-auto select-none pointer-events-none
