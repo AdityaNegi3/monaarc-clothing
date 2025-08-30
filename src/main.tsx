234508b012f30db1fd90e9627cb1bff7076b5b54
AdityaNegi3 <adi.negi730@gmail.com>
2025-08-30 16:58:14 +0530

    hi

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 
-const KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string | undefined;
+// Import your Clerk Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key. Add it to your .env file");
+}
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {KEY ? (
-      <ClerkProvider publishableKey={KEY} afterSignOutUrl="/">
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ClerkProvider>
-    ) : (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    )}
+    </ClerkProvider>
   </React.StrictMode>
 );
